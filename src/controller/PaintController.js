4c9cc57a784f5522cf3b4a87b26db324364f036a
Niko Kaartinen <niko.kaartinen@protonmail.com>
2019-07-05 16:14:57 +0300

    Add token check

--- src/controller/PaintController.js
+++ src/controller/PaintController.js
@@ -3,22 +3,33 @@ const Paint = require('../model/Paint');
 
 const router = express.Router();
 
+require('dotenv').config();
+const token = process.env.TOKEN;
+
+const isTokenValid = reqToken => {
+	return reqToken === token ? true : false;
+};
+
 router.post('/addpaint', (req, res) => {
-	try {
-		Paint.findAll({ where: { name: req.body.name } }).then(paints => {
-			if (paints.length > 0) {
-				res.status(400).send('duplicate entry found');
-			} else {
-				Paint.create({
-					name: req.body.name,
-					amount: req.body.amount
-				});
-				res.status(200).send('Paint added succesfully');
-			}
-		});
-	} catch (error) {
-		console.log(error);
-		res.status(500).send('Unexpected error');
+	if (!isTokenValid(req.body.token)) {
+		res.status(401).send('Invalid token');
+	} else {
+		try {
+			Paint.findAll({ where: { name: req.body.name } }).then(paints => {
+				if (paints.length > 0) {
+					res.status(400).send('duplicate entry found');
+				} else {
+					Paint.create({
+						name: req.body.name,
+						amount: req.body.amount
+					});
+					res.status(200).send('Paint added succesfully');
+				}
+			});
+		} catch (error) {
+			console.log(error);
+			res.status(500).send('Unexpected error');
+		}
 	}
 });
 
@@ -34,37 +45,45 @@ router.get('/getpaints', (req, res) => {
 });
 
 router.post('/updatepaint', (req, res) => {
-	try {
-		Paint.findByPk(req.body.id).then(paint => {
-			if (paint !== null) {
-				paint.update({
-					name: req.body.name,
-					amount: req.body.amount
-				});
-				res.status(200).send('Paint updated succesfully');
-			} else {
-				res.status(404).send('Paint not found');
-			}
-		});
-	} catch (error) {
-		console.log(e);
-		res.status(500).send('Unexpected error');
+	if (!isTokenValid(req.body.token)) {
+		res.status(401).send('Invalid token');
+	} else {
+		try {
+			Paint.findByPk(req.body.id).then(paint => {
+				if (paint !== null) {
+					paint.update({
+						name: req.body.name,
+						amount: req.body.amount
+					});
+					res.status(200).send('Paint updated succesfully');
+				} else {
+					res.status(404).send('Paint not found');
+				}
+			});
+		} catch (error) {
+			console.log(e);
+			res.status(500).send('Unexpected error');
+		}
 	}
 });
 
 router.post('/deletepaint', (req, res) => {
-	try {
-		Paint.findByPk(req.body.id).then(paint => {
-			if (paint !== null) {
-				paint.destroy();
-				res.status(200).send('Paint deleted succesfully');
-			} else {
-				res.status(404).send('Paint not found');
-			}
-		});
-	} catch (error) {
-		console.log(e);
-		res.status(500).send('Unexpected error');
+	if (!isTokenValid(req.body.token)) {
+		res.status(401).send('Invalid token');
+	} else {
+		try {
+			Paint.findByPk(req.body.id).then(paint => {
+				if (paint !== null) {
+					paint.destroy();
+					res.status(200).send('Paint deleted succesfully');
+				} else {
+					res.status(404).send('Paint not found');
+				}
+			});
+		} catch (error) {
+			console.log(e);
+			res.status(500).send('Unexpected error');
+		}
 	}
 });
 
