eb51404a27fc13e1b0b0806f1ddd2158050e354a
Niko Kaartinen <niko.kaartinen@protonmail.com>
2019-07-05 12:54:34 +0300

    Use .env file, add it to .gitignore

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+
+require('dotenv').config();
 
 app.get('/', (req, res) => res.send('Hello world!'));
 
-app.listen(port, () => console.log(`App listening on port ${port}!`));
+app.listen(process.env.PORT, () => console.log(`App listening on port ${process.env.PORT}!`));
